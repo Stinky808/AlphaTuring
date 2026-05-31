903823ea3de9c298c1d38aa698741aac2050c9e1
Anthony Duenez Ramirez <duenezanthony67@gmail.com>
2026-05-30 20:33:06 -0700

    implemented supersonic sensor

--- arduino/motor_serial.ino
+++ arduino/motor_serial.ino
@@ -3,7 +3,15 @@ const int IN2 = 6;
 const int IN3 = 9;
 const int IN4 = 10;
 
+const int TRIG_PIN = 11;
+const int ECHO_PIN = 12;
+
+const int TOO_CLOSE_CM = 35;
+
 String command = "";
+
+unsigned long lastCommandTime = 0;
+const unsigned long COMMAND_TIMEOUT = 700;
 
 void setup() {
   Serial.begin(9600);
@@ -13,6 +21,9 @@ void setup() {
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
 
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
   stopMotors();
 }
 
@@ -21,18 +32,60 @@ void loop() {
     command = Serial.readStringUntil('\n');
     command.trim();
 
-    if (command == "FORWARD") {
-      forward();
-    } else if (command == "BACKWARD") {
-      backward();
-    } else if (command == "LEFT") {
-      left();
-    } else if (command == "RIGHT") {
-      right();
-    } else {
-      stopMotors();
-    }
+    lastCommandTime = millis();
+
+    handleCommand(command);
   }
+
+  if (millis() - lastCommandTime > COMMAND_TIMEOUT) {
+    stopMotors();
+  }
+}
+
+void handleCommand(String cmd) {
+  int distance = readDistanceCm();
+
+  if (cmd == "FORWARD") {
+    if (distance < 0 || distance <= TOO_CLOSE_CM) {
+      stopMotors();
+      Serial.println("BLOCKED_TOO_CLOSE");
+    } else {
+      forward();
+    }
+  } 
+  else if (cmd == "BACKWARD") {
+    backward();
+  } 
+  else if (cmd == "LEFT") {
+    left();
+  } 
+  else if (cmd == "RIGHT") {
+    right();
+  } 
+  else if (cmd == "STOP") {
+    stopMotors();
+  } 
+  else {
+    stopMotors();
+  }
+}
+
+int readDistanceCm() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH, 30000);
+
+  if (duration == 0) {
+    return -1;
+  }
+
+  int distance = duration * 0.034 / 2;
+  return distance;
 }
 
 void forward() {
